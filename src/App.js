2d7cd83ebaaeb4b27fca4f0b741e54ed4dc96fad
ashutosh4036 <ashutosh.tripathi_cs19@gla.ac.in>
2022-08-20 13:07:47 +0530

    Designed editors page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-
+import { BrowserRouter,Routes,Route} from 'react-router-dom';
 import './App.css';
+import './index.css'
+import {Toaster} from 'react-hot-toast'
+import EditorPage from './pages/EditorPage';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <>
+     <div>
+      <Toaster
+       position='top-right'
+       toastOptions={{
+        success:{
+          theme:{
+            primary:'#4aed88',
+          },
+        },
+       }}
+       >
 
-    </div>
+      </Toaster>
+     </div>
+       <BrowserRouter>
+         <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/editor/:roomId" element={<EditorPage/>}></Route>
+         </Routes>
+       </BrowserRouter>
+    </>
   );
 }
 
