fcbe2e890787a3eae29eba3561d074f8f813cb9b
ashutosh4036 <ashutosh.tripathi_cs19@gla.ac.in>
2022-08-21 11:46:23 +0530

    Made some changes

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,15 +1,17 @@
-import React ,{useEffect}from 'react'
+import React, { useEffect, useRef } from 'react';
 import Codemirror from 'codemirror';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/dracula.css';
 import 'codemirror/mode/javascript/javascript';
 import 'codemirror/addon/edit/closetag';
 import 'codemirror/addon/edit/closebrackets';
-const Editor = () => {
-  
-    useEffect(()=>{
+import ACTIONS from '../Actions';
+
+const Editor = ({ socketRef, roomId, onCodeChange }) => {
+    const editorRef = useRef(null);
+    useEffect(() => {
         async function init() {
-            Codemirror.fromTextArea(
+            editorRef.current = Codemirror.fromTextArea(
                 document.getElementById('realtimeEditor'),
                 {
                     mode: { name: 'javascript', json: true },
@@ -19,13 +21,37 @@ const Editor = () => {
                     lineNumbers: true,
                 }
             );
-    } 
-      init();
-    },[]);
 
-  return (
-    <div></div>
-  )
-}
+            editorRef.current.on('change', (instance, changes) => {
+                const { origin } = changes;
+                const code = instance.getValue();
+                onCodeChange(code);
+                if (origin !== 'setValue') {
+                    socketRef.current.emit(ACTIONS.CODE_CHANGE, {
+                        roomId,
+                        code,
+                    });
+                }
+            });
+        }
+        init();
+    }, []);
 
-export default Editor
+    useEffect(() => {
+        if (socketRef.current) {
+            socketRef.current.on(ACTIONS.CODE_CHANGE, ({ code }) => {
+                if (code !== null) {
+                    editorRef.current.setValue(code);
+                }
+            });
+        }
+
+        return () => {
+            socketRef.current.off(ACTIONS.CODE_CHANGE);
+        };
+    }, [socketRef.current]);
+
+    return <textarea id="realtimeEditor"></textarea>;
+};
+
+export default Editor;
